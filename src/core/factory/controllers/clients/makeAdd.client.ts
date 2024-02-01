f8c98af140a231d8d46bc85c79a7bfd453edca64
Rafael Torreson <rafael.torreson@hotmail.com>
2024-02-01 00:15:34 -0300

    feat: add validator data from body

--- src/core/factory/controllers/clients/makeAdd.client.ts
+++ src/core/factory/controllers/clients/makeAdd.client.ts
@@ -1,9 +1,15 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { AddClientHandler } from '@/app';
+import { ClientDataValidator } from '@/validators/client/client.validation';
+import { ValidatorAdapter } from '@/core/adapter';
 
 export const makeAddClientController = (
   request: Request,
-  response: Response
+  response: Response,
+  next: NextFunction
 ) => {
-  return new AddClientHandler().handle(request, response);
+  const clientDataValidator = new ClientDataValidator();
+  const validatorAdapter = new ValidatorAdapter(clientDataValidator.schema);
+  const addClientHandler = new AddClientHandler(validatorAdapter);
+  return addClientHandler.handle(request, response, next);
 };
