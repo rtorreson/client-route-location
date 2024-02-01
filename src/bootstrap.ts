5ff64533b2f53ea3bb7ad21c6dbf09876d83df65
Rafael Torreson <rafael.torreson@hotmail.com>
2024-01-31 21:53:22 -0300

    feat: server instance initializate

---
+++ src/bootstrap.ts
@@ -0,0 +1,25 @@
+import { Server } from '@/core/server';
+import { Logger } from '@/layer/middlewares';
+import { ServerError } from '@/layer/errors';
+import ENV from '@/infra/environments'
+import { internalProcess } from '@/infra/process';
+
+const bootstrap = async () => {
+    const logger = new Logger();
+    try {
+        const serverInstance = new Server({
+            port: ENV.SERVER.PORT,
+        });
+
+        internalProcess(serverInstance)
+
+        serverInstance.start();
+    } catch (error) {
+        if (error instanceof ServerError) {
+            logger.warn(`Erro ao iniciar ${error.message}`);
+            process.exit(1);
+        }
+    }
+};
+
+bootstrap();
