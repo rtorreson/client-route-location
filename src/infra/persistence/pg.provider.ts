e1ba0d10fe4ea4ac0f561976fdba7395e5726684
Rafael Torreson <rafael.torreson@hotmail.com>
2024-01-31 22:46:08 -0300

    fix(feat): add type to return data row

--- src/infra/persistence/pg.provider.ts
+++ src/infra/persistence/pg.provider.ts
@@ -1,47 +1,61 @@
 import { Pool, PoolClient, QueryResultRow } from 'pg';
-import ENV from '@/infra/environments'
+import ENV from '@/infra/environments';
+import { GlobalError } from '@/layer/errors';
+import { Logger } from '@/layer';
 
 class PgConnection {
-    private static instance: PgConnection;
-    private pool: Pool;
+  private static instance: PgConnection;
+  private pool: Pool;
+  private readonly logger: Logger;
 
-    private constructor() {
-        this.pool = new Pool({
-            user: ENV.DATABASE.PG.USER,
-            host: ENV.DATABASE.PG.HOST,
-            database: ENV.DATABASE.PG.DB,
-            password: ENV.DATABASE.PG.PASS,
-            port: ENV.DATABASE.PG.PORT,
-        });
-    }
+  private constructor() {
+    this.pool = new Pool({
+      user: ENV.DATABASE.PG.USER,
+      host: ENV.DATABASE.PG.HOST,
+      database: ENV.DATABASE.PG.DB,
+      password: ENV.DATABASE.PG.PASS,
+      port: ENV.DATABASE.PG.PORT,
+    });
 
-    static getInstance(): PgConnection {
-        if (!PgConnection.instance) {
-            PgConnection.instance = new PgConnection();
-        }
-        return PgConnection.instance;
-    }
+    this.logger = new Logger();
+  }
 
-    async getClient(): Promise<PoolClient> {
-        const client = await this.pool.connect();
-        return client;
+  static getInstance(): PgConnection {
+    if (!PgConnection.instance) {
+      PgConnection.instance = new PgConnection();
     }
+    return PgConnection.instance;
+  }
 
-    async release(client: PoolClient): Promise<void> {
-        client.release();
-    }
+  async getClient(): Promise<PoolClient> {
+    const client = await this.pool.connect();
+    return client;
+  }
 
-    async query<T extends QueryResultRow>(sql: string, values?: any[]): Promise<T[] | undefined> {
-        const client = await this.pool.connect();
-        try {
-            const result = await client.query<T>(sql, values);
-            return result.rows;
-        } catch (error: unknown) {
-            console.error(error)
-        } finally {
-            this.release(client);
-        }
+  async release(client: PoolClient): Promise<void> {
+    client.release();
+  }
+
+  async query<T extends QueryResultRow>({
+    sql,
+    values,
+  }: {
+    sql: string;
+    values?: any[];
+  }): Promise<QueryResultRow[]> {
+    const client = await this.pool.connect();
+    try {
+      const result = await client.query<T>(sql, values);
+      return result.rows;
+    } catch (error: unknown) {
+      if (error instanceof GlobalError) {
+        this.logger.error(error.message);
+      }
+      throw error;
+    } finally {
+      this.release(client);
     }
+  }
 }
 
 export default PgConnection;
